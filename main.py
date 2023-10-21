b9e20e241f0d3159ec07e78a87f5f287f3f210a2
J Prapanch <prapanchappuse789@gmail.com>
2023-10-21 19:31:52 +0530

    completed-onboarding page 1

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "MediFlet"
     page.bgcolor=ft.colors.BLUE_50
-    page.add(ft.Text("MediFlet"))
+    page.add(ft.Text("MediFlet",color= ft.colors.INDIGO_500,size=50)) 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(ft.IconButton(icon=ft.icons.ARROW_FORWARD,icon_color=ft.colors.INDIGO_500,icon_size=25))
     page.update()
+   
 
-
+ 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main) 
