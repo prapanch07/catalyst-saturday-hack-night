3475e8e2dceea315747ae53eef2ead501d946896
J Prapanch <prapanchappuse789@gmail.com>
2023-10-21 19:46:33 +0530

    added-test.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Lambda
 import flet as ft
 
 def main(page: ft.Page):
@@ -6,7 +7,8 @@ def main(page: ft.Page):
     page.add(ft.Text("MediFlet",color= ft.colors.INDIGO_500,size=50)) 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(ft.IconButton(icon=ft.icons.ARROW_FORWARD,icon_color=ft.colors.INDIGO_500,icon_size=25))
+    page.add(ft.IconButton(icon=ft.icons.ARROW_FORWARD,icon_color=ft.colors.INDIGO_500,icon_size=25,on_click= Lambda:page.go("/test.py")))
+    
     page.update()
    
 
